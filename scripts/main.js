b23dc53c48af4239c887f3d98577aba58bfc4866
Luke Walker <lukeww101@gmail.com>
2023-06-06 19:22:15 +0100

    Fixed bug, changed func names, organised file

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,66 @@
+window.onload = init;
+
+function init() {
+  document.getElementById("input").value = "";
+}
+
+function start() {
+  sentence = makeSentence();
+  points = 0;
+
+  playAnim(document.getElementById("current"), "fade")
+  playAnim(document.getElementById("next"), "fade")
+
+  document.getElementById("current").innerHTML = sentence[0];
+  document.getElementById("next").innerHTML = sentence[1];
+  document.getElementById("score").innerHTML = points;
+  document.getElementById("input").onclick = "";
+}
+
+// I dont like how messy this is but it works ¯\_(ツ)_/¯
+function update() {
+  let current = document.getElementById("current").innerHTML;
+  let next = document.getElementById("next").innerHTML;
+  let points = document.getElementById("score").innerHTML;
+
+  sentence = [current, next];
+  input = (document.getElementById("input").value).toLowerCase()
+
+  if (input == sentence[0]) {
+    points++;
+    prev = sentence[0];
+    sentence.shift();
+    sentence.push(getWord(sentence));
+    playAnim(document.getElementById("score"), "fx");
+    playAnim(document.getElementById("input"), "fx", function () {
+      document.getElementById("input").value = "";
+    });
+    playAnim(document.getElementById("next"), "fx");
+    playAnim(document.getElementById("prev"), "fx");
+    playAnim(document.getElementById("current"), "fx", function () {
+      write(points, sentence, prev);
+    });
+  }
+}
+
+function playAnim(element, name, callback) {
+  element.classList.add(name);
+  element.addEventListener('animationend', () => {
+    element.classList.remove(name);
+    callback();
+  });
+}
+
 function getWord(sentence) {
   word = wordList[Math.floor(Math.random() * wordList.length)];
 
-  if (word == sentence[1] || word.length > 10) {
+  if (word == sentence[0] || word == sentence[1] || word.length > 10) {
     getWord(sentence)
   }
   return word;
 }
 
-function generateSentence() {
+function makeSentence() {
   sentence = [];
   const wordCount = 2;
   for (let i = 0; i < wordCount; i++) {
@@ -16,52 +69,9 @@ function generateSentence() {
   return sentence;
 }
 
-function write(points, sentence) {
+function write(points, sentence, prev) {
+  document.getElementById("prev").innerHTML = prev;
   document.getElementById("current").innerHTML = sentence[0];
   document.getElementById("next").innerHTML = sentence[1];
   document.getElementById("score").innerHTML = points;
-}
-
-function start() {
-  sentence = generateSentence();
-  points = 0;
-  animate(document.getElementById("current"), "fade")
-  animate(document.getElementById("next"), "fade")
-  document.getElementById("current").innerHTML = sentence[0];
-  document.getElementById("next").innerHTML = sentence[1];
-  document.getElementById("score").innerHTML = points;
-  document.getElementById("input").onclick = "";
-}
-
-// I dont like how messy this is but it works ¯\_(ツ)_/¯
-function processInput() {
-  let current = document.getElementById("current").innerHTML;
-  let next = document.getElementById("next").innerHTML;
-  let points = document.getElementById("score").innerHTML;
-  sentence = [current, next];
-  input = (document.getElementById("input").value).toLowerCase()
-  if (input == sentence[0]) {
-    points++;
-    prev = sentence[0];
-    sentence.shift();
-    sentence.push(getWord(sentence));
-    animate(document.getElementById("score"), "fx");
-    animate(document.getElementById("input"), "fx", function () {
-      document.getElementById("input").value = "";
-    });
-    animate(document.getElementById("next"), "fx");
-    animate(document.getElementById("prev"), "fx");
-    animate(document.getElementById("current"), "fx", function () {
-      document.getElementById("prev").innerHTML = prev;
-      write(points, sentence);
-    });
-  }
-}
-
-function animate(element, name, callback) {
-  element.classList.add(name);
-  element.addEventListener('animationend', () => {
-    element.classList.remove(name);
-    callback();
-  });
 }
